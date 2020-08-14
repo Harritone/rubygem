c6d6e0790d565b59d1e67890d8ada321b1f978cb
Harritone <yourrasik@gmail.com>
2020-08-14 18:48:46 +1000

    Implemented user model and added relatonship to courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
+User.create!(email: 'test@test.com', password: '111111', password_confirmation: '111111')
+
+puts "Admin created"
+
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::ChuckNorris.fact
+    description: Faker::ChuckNorris.fact,
+    user_id: User.first.id
   }])
 end
 
