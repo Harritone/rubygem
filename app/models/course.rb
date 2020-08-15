cddb95a2f3d53f25438785cfa06836467cdf1fc0
Harritone <yourrasik@gmail.com>
2020-08-15 12:29:30 +1000

    Implemented friendly_id for courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,16 @@ class Course < ApplicationRecord
   belongs_to :user
   has_rich_text :description
 
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  #friendly_id :generated_slug, use: :slugged
+
+  def generated_slug
+    require 'securerandom'
+    @random_slag ||= persisted? ? friendly_id : SecureRandom.hex(4)
+  end
+
   def to_s
     title
   end
