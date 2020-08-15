7df86a6c8aad68869a4c1b9cc3a63554e573d6ae
Harritone <yourrasik@gmail.com>
2020-08-15 17:26:44 +1000

    Added additional fields for coursesw

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-User.create!(email: 'test@test.com', password: '111111', password_confirmation: '111111')
+#User.create!(email: 'test@test.com', password: '111111', password_confirmation: '111111')
 
 puts "Admin created"
 
@@ -6,7 +6,12 @@ puts "Admin created"
   Course.create!([{
     title: Faker::Educator.course_name,
     description: Faker::ChuckNorris.fact,
-    user_id: User.first.id
+    user_id: User.first.id,
+    short_description: Faker::Quote.most_interesting_man_in_the_world,
+    level: "Begginer",
+    language: "#{["Russian", "Spanish", "English", "French"].sample}",
+    price: Faker::Number.between(from: 1000, to: 20000)
+
   }])
 end
 
