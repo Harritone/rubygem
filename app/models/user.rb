450cb2e9ccd57f1e49f5d9a62d31c187efafe5e2
Harritone <yourrasik@gmail.com>
2020-08-16 22:37:42 +1000

    Implemented roles for users with rolify

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :trackable, :confirmable
 
+  rolify
+
   has_many :courses
 
   def username
@@ -19,5 +21,18 @@ class User < ApplicationRecord
 
   def to_s
     email
+  end
+
+    after_create :assign_default_role
+
+  def assign_default_role
+    if User.count == 1
+      self.add_role(:admin) if self.role.blank?
+      self.add_role(:teacher)
+      self.add_role(:student)
+    else
+      self.add_role(:student)
+      self.add_role(:teacher)
+    end
   end
 end
