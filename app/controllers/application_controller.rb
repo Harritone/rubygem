732c615f9a1b340f4ae3668e79efeffdf3e9f056
Harritone <yourrasik@gmail.com>
2020-08-17 11:27:23 +1000

    Implemented basic authorization for courses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   before_action :authenticate_user!
 
   include PublicActivity::StoreController # => save current_user using gem public activity
@@ -7,5 +11,12 @@ class ApplicationController < ActionController::Base
 
   def set_global_variables
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
   end
 end
