1aed527d7f4497928f55d97c61dce5268cea3685
Harritone <yourrasik@gmail.com>
2020-08-18 02:21:01 +1000

    Added action text fot lesson content

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ApplicationRecord
   validates :title, :short_description, :language, :price, :level, presence: true
   validates :description, presence: true, length: { minimum: 5 }
+
   belongs_to :user
   has_many :lessons, dependent: :destroy
   has_rich_text :description
