0736d2f9cac1b70761d73e1a69dd0e2978177ef8
Harritone <yourrasik@gmail.com>
2020-08-19 20:59:02 +1000

    Added logic to enrollments and created button

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,18 @@
 module CoursesHelper
+
+  def enrollment_button(course)
+    if current_user
+      if course.user == current_user
+        link_to "Analitics", course_path(course)
+      elsif course.enrollments.where(user: current_user).any?
+        link_to "Go to course", course_path(course), class: 'btn btn-primary'
+      elsif course.price > 0
+        link_to number_to_currency(course.price), new_course_enrollment_path(course), class: 'btn btn-success'
+      else
+        link_to "Free", new_course_enrollment_path(course), class: 'btn btn-info'
+      end
+    else
+      link_to "Check price", course_path(course), class: 'btn btn-md btn-success'
+    end
+  end
 end
