edc7256423ca60428d3621e8c130c0512f90c11d
Harritone <yourrasik@gmail.com>
2020-08-20 01:22:28 +1000

    Implemented friendly id for enrollments

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -8,6 +8,9 @@ class Enrollment < ApplicationRecord
 
   validate :cant_subscribe_to_own_course
 
+  extend FriendlyId
+  friendly_id :to_s, use: :slugged
+
   scope :pending_review, -> { where(rating: [0, nil, ""], review: [0, nil, ""]) }
 
   def to_s
